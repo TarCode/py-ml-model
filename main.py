550edb0f263e6600925b5b9c4dffd89d1ef98fa0
Taariq Isaacs <taariq@aerobotics.com>
2020-02-20 16:45:36 +0200

    used random forest regressor on new csv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -10,22 +10,21 @@ from sklearn.model_selection import train_test_split
 
 
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model = RandomForestRegressor(max_leaf_nodes=max_leaf_nodes, random_state=1)
     model.fit(train_X, train_y)
     preds_val = model.predict(val_X)
-    print('Prediction value', preds_val)
     mae = mean_absolute_error(val_y, preds_val)
     return mae
 
 
-data_path = './property-listings.csv'
+data_path = './property-listing.csv'
 data = pd.read_csv(data_path, dtype={"Close": float})
 data = data.dropna(axis=0)
 
 print('PRICE COLUMNS')
 print(data.columns)
 
-features = ['title', 'location', 'area', 'price']
+features = ['bedrooms', 'area', 'price']
 
 X = data[features]
 y = data.price
@@ -36,7 +35,7 @@ train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 print('DATA DESCRIBE')
 print(X.describe())
 
-model = DecisionTreeRegressor(random_state=1)
+model = RandomForestRegressor(random_state=1)
 
 model.fit(X, y)
 
@@ -50,6 +49,6 @@ print(predicted_prices)
 print('MEAN ABSOLUTE ERROR')
 print(mean_absolute_error(y, predicted_prices))
 
-for max_leaf_nodes in [2, 3, 5, 8, 10, 2000]:
+for max_leaf_nodes in [500, 5000, 50000]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print('Max leaf nodes: %d \t\t Mean absolute error: %d' % (max_leaf_nodes, my_mae))
