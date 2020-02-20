dbc7fb9f1c2242173cfeb7081c9d90cb98fc45c0
Taariq Isaacs <taariq@aerobotics.com>
2020-02-20 12:49:26 +0200

    split training and validation data, check accuracy with different max leaf nodes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pandas as pd
-import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    print('Prediction value', preds_val)
+    mae = mean_absolute_error(val_y, preds_val)
+    return mae
+
 
 crypto_price_path = './crypto_prices.csv'
 crypto_data = pd.read_csv(crypto_price_path, dtype={"Close": float})
@@ -14,6 +24,8 @@ features = ['Open', 'Close', 'Volume', 'High', 'Low']
 
 X = crypto_data[features]
 y = crypto_data.Close
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
 print('CRYPTO DATA COLUMNS')
 print(X.columns)
@@ -34,3 +46,7 @@ print(predicted_crypto_closing_prices)
 
 print('MEAN ABSOLUTE ERROR')
 print(mean_absolute_error(y, predicted_crypto_closing_prices))
+
+for max_leaf_nodes in [2, 3, 5, 8, 10, 2000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print('Max leaf nodes: %d \t\t Mean absolute error: %d' % (max_leaf_nodes, my_mae))
