d2bdb751d2ad59d40c1222045865fba8423bc944
Taariq Isaacs <taariq@aerobotics.com>
2020-03-06 14:57:05 +0200

    added XGBRegressor

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_absolute_error
 from location_order import LOCATIONS
+
 
 def preprocess_data(data_path):
     data = pd.read_csv(data_path)
@@ -18,7 +21,7 @@ def preprocess_data(data_path):
     for index, location in enumerate(LOCATIONS):
         data.replace(location, index, inplace=True)
 
-    features = ['bedrooms', 'type', 'location', 'area', 'bathrooms', 'parking', 'price']
+    features = ['bedrooms', 'type', 'location', 'area', 'bathrooms', 'parking']
 
     X = data[features]
     y = data.price
@@ -48,19 +51,32 @@ def get_scores_for_max_leaf_nodes(max_leaf_nodes, X_train, y_train):
     return scores.mean()
 
 
+def get_scores_with_xg_boost(n_estimators, X_train, y_train, X_test, y_test):
+    model = XGBRegressor(n_estimators=n_estimators, learning_rate=0.1, n_jobs=2)
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+    mae = mean_absolute_error(y_test, predictions)
+    return "Mean Absolute Error %f" % mae
+
+
 X_train, X_test, y_train, y_test = preprocess_data('data/cape-town-property-listings.csv')
+#
+# max_leaf_nodes_results = {}
+# for i in [34, 55, 89, 144, 233, 377, 610, 947]:
+#     max_leaf_nodes_results[i] = get_scores_for_max_leaf_nodes(i, X_train, y_train)
+#
+# n_estimators_results = {}
+#
+# for i in range(400, 950, 50):
+#     n_estimators_results[i] = get_scores_for_n_estimators(i, X_train, y_train)
 
-max_leaf_nodes_results = {}
-for i in [34, 55, 89, 144, 233, 377, 610, 947]:
-    max_leaf_nodes_results[i] = get_scores_for_max_leaf_nodes(i, X_train, y_train)
+# print('MAX LEAF NODES RESULTS')
+# print(max_leaf_nodes_results)
+#
+# print('N ESTIMATORS RESULTS')
+# print(n_estimators_results)
 
-n_estimators_results = {}
-
-for i in range(400, 950, 50):
-    n_estimators_results[i] = get_scores_for_n_estimators(i, X_train, y_train)
-
-print('MAX LEAF NODES RESULTS')
-print(max_leaf_nodes_results)
-
-print('N ESTIMATORS RESULTS')
-print(n_estimators_results)
+xgboost_results = get_scores_with_xg_boost(5000, X_train, y_train, X_test, y_test)
+print('XG BOOST RESULTS')
+print(xgboost_results)
